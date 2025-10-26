5277d8a1d295e8c795d6f54fc8cce539b7de08bd
victormmendes <victormmendes.vm@icloud.com>
2025-10-26 15:05:47 -0300

    Refining the Project

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,17 +2,19 @@ require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sessions_new_url
+    get new_sessions_path
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "should create session" do
+    # Create a user with a valid bcrypt password
+    user = User.create!(name: "Login User", email: "login@example.com", password: "password", password_confirmation: "password")
+    post sessions_path, params: { email: user.email, password: "password" }
+    assert_response :redirect
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+  test "should destroy session" do
+    delete sessions_path
+    assert_response :redirect
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest } }
+      post users_url, params: { user: { email: "new@example.com", name: "New User", password: "secret123", password_confirmation: "secret123" } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest } }
+    patch user_url(@user), params: { user: { email: "updated@example.com", name: "Updated Name" } }
     assert_redirected_to user_url(@user)
   end
 
